449d23984c90d6996df633dfc1ba8df551e0235c
RangerLeonardo <brandonleonardobarrera@gmail.com>
2024-04-27 22:51:03 -0600

    Agregando archivos base

---
+++ Cascaron Portafolio/scripts.js
@@ -0,0 +1,30 @@
+const inicio = document.getElementById("inicio");
+const sobreMi = document.getElementById("sobre_mi");
+const habilidadesBlandas = document.getElementById("habilidades_blandas");
+const proyectos = document.getElementById("proyectos");
+const contactame = document.getElementById("contactame");
+const english = document.getElementById("english");
+
+inicio.addEventListener("click", function(){
+    console.log("Isidro se la come");
+});
+
+sobreMi.addEventListener("click", function(){
+    console.log("Isidro se la come");
+});
+
+habilidadesBlandas.addEventListener("click", function(){
+    console.log("Isidro se la come");
+});
+
+proyectos.addEventListener("click", function(){
+    console.log("Isidro se la come");
+});
+
+contactame.addEventListener("click", function(){
+    console.log("Isidro se la come");
+});
+
+english.addEventListener("click", function(){
+    console.log("Isidro se la come");
+});
